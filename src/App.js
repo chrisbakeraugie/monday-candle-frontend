25074dc6a8fd982cccbce58c614a977acb0992c4
Chris Baker <chrisbaker@martins-imac.myfiosgateway.com>
2024-03-25 11:04:15 -0400

    Big updates to submit order functionality, creates/mutates items on the current board. Notifies user of errors and successes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
@@ -11,7 +11,10 @@ import StartOrder from "./components/forms/StartOrder";
 const monday = mondaySdk();
 
 const App = () => {
-  const { setMondayContext, setFragrances } = useAppContext();
+  const { setMondayContext, setFragrances, mondayContext } = useAppContext();
+  const [isGroupCreationStarted, setIsGroupCreationStarted] = useState(false);
+  const [isBoardColumnsCreationStarted, setIsBoardColumnsCreationStarted] =
+    useState(false);
 
   useEffect(() => {
     // Notice this method notifies the monday platform that user gains a first value in an app.
@@ -22,19 +25,109 @@ const App = () => {
     monday.listen("context", (res) => {
       setMondayContext(res.data);
     });
+  }, [setFragrances, setMondayContext]);
 
+  useEffect(() => {
+    const currentBoardId = mondayContext?.data?.boardId ?? 6319041765; // TODO remove this reference
     const fetchFragrances = async () => {
       try {
         const response = await axios.get("http://localhost:3000/fragrance/");
         setFragrances(response.data);
       } catch (error) {
-        console.log(error);
-        console.error("Could not fetch fragrances");
+        console.error("Could not fetch fragrances", error);
       }
     };
 
+    const checkAndCreateColumns = async () => {
+      setIsBoardColumnsCreationStarted(true);
+      try {
+        const columnsQuery = `query ($boardId: ID!) { boards(ids: [$boardId]) { columns { id title }  } }`;
+        const existingColumnsResponse = await monday.api(columnsQuery, {
+          variables: { boardId: currentBoardId },
+        });
+        const existingColumns = existingColumnsResponse.data.boards[0].columns;
+        const existingColumnTitles = existingColumns.map(
+          (column) => column.title
+        );
+
+        const createColumnMutation = `mutation createColumn($boardId: ID!, $title: String!, $columnType: ColumnType!) {
+          create_column(board_id: $boardId, title: $title, column_type: $columnType) {
+            id
+          }
+        }`;
+
+        const columnDetails = [
+          { title: "Sales Associate", columnType: "text" },
+          { title: "Inscription", columnType: "text" },
+          {
+            title: "Status",
+            columnType: "status",
+          },
+          { title: "Scent Profile", columnType: "text" },
+          { title: "Quantity", columnType: "numbers" },
+        ];
+
+        for (const { title, columnType } of columnDetails) {
+          if (!existingColumnTitles.includes(title)) {
+            await monday.api(createColumnMutation, {
+              variables: { boardId: currentBoardId, title, columnType },
+            });
+          }
+        }
+      } catch (error) {
+        console.error("Error checking columns:", error);
+      }
+    };
+
+    const checkAndCreateGroups = async () => {
+      setIsGroupCreationStarted(true);
+      const query = `query ($boardId: [ID!]) {
+        boards(ids: $boardId) {
+          groups {
+            id
+            title
+          }
+        }
+      }`;
+
+      try {
+        const existingGroupsResponse = await monday.api(query, {
+          variables: { boardId: [currentBoardId] },
+        });
+        const existingGroups = existingGroupsResponse.data.boards[0].groups;
+        const requiredGroups = ["Done", "Working On It", "Received"];
+        const existingGroupTitles = existingGroups.map((group) => group.title);
+
+        for (let groupName of requiredGroups) {
+          if (!existingGroupTitles.includes(groupName)) {
+            const createGroupMutation = `mutation ($boardId: ID!, $groupName: String!) {
+              create_group(board_id: $boardId, group_name: $groupName) {
+                id
+              }
+            }`;
+
+            await monday.api(createGroupMutation, {
+              variables: { boardId: currentBoardId, groupName }, // Corrected variable name
+            });
+          }
+        }
+      } catch (error) {
+        console.error("Error checking/creating groups:", error);
+      }
+    };
+    if (currentBoardId && !isBoardColumnsCreationStarted) {
+      checkAndCreateColumns();
+    }
+    if (currentBoardId && !isGroupCreationStarted) {
+      checkAndCreateGroups();
+    }
     fetchFragrances();
-  }, [setFragrances, setMondayContext]);
+  }, [
+    isBoardColumnsCreationStarted,
+    isGroupCreationStarted,
+    mondayContext,
+    setFragrances,
+  ]);
 
   return (
     <div className="App">
--- src/components/forms/StartOrder.jsx
+++ src/components/forms/StartOrder.jsx
@@ -1,14 +1,33 @@
-import { Button, Dropdown, Flex, Label, TextField } from "monday-ui-react-core";
-import { useMemo } from "react";
+import {
+  Box,
+  Button,
+  Dropdown,
+  Flex,
+  Label,
+  TextField,
+} from "monday-ui-react-core";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useAppContext } from "../../state/AppContext";
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log("SUbmitted");
-};
+import mondaySdk from "monday-sdk-js";
+const monday = mondaySdk();
 
 const StartOrder = () => {
-  const { fragrances } = useAppContext();
+  const { fragrances, mondayContext } = useAppContext();
+  const [formData, setFormData] = useState({
+    orderName: "",
+    selectedFragrances: [],
+    firstName: "",
+    lastName: "",
+    quantity: "",
+    inscription: "",
+  });
+  const [displayLimit, setDisplayLimit] = useState(false);
+  const [successLabel, setSuccessLabel] = useState(false);
+  const [failLabel, setFailLabel] = useState(false);
+  const [receivedGroupId, setReceivedGroupId] = useState(null);
+  const [disableSubmit, setDisableSubmit] = useState(false);
+  const currentBoardId = mondayContext?.data?.boardId ?? 6319041765; // TODO remove this reference
+  const previousOrderRef = useRef(null);
 
   const fragranceOptions = useMemo(() => {
     const formattedOptions = fragrances
@@ -27,19 +46,204 @@ const StartOrder = () => {
       });
     return formattedOptions;
   }, [fragrances]);
+
+  const handleSelectChange = (selectedItem, event) => {
+    switch (event.action) {
+      case "clear":
+        setFormData((prev) => ({ ...prev, selectedFragrances: [] }));
+        break;
+      case "select-option":
+        if (selectedItem.length <= 3) {
+          setFormData((prev) => ({
+            ...prev,
+            selectedFragrances: selectedItem,
+          }));
+        } else {
+          setDisplayLimit(true);
+        }
+        break;
+      default:
+        setFormData((prev) => ({ ...prev, selectedFragrances: selectedItem }));
+    }
+    if (formData.selectedFragrances.length > 3) {
+      return;
+    }
+  };
+
+  const handleTextFieldChange = (value, field) => {
+    setSuccessLabel(false);
+    setFailLabel(false);
+
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  useEffect(() => {
+    if (!currentBoardId) return;
+    const getReceivedGroupId = async () => {
+      const query = `query getBoardGroups($boardId: ID!) {
+        boards(ids: [$boardId]) {
+          groups {
+            id
+            title
+          }
+        }
+      }`;
+
+      try {
+        const response = await monday.api(query, {
+          variables: { boardId: currentBoardId },
+        });
+        const groups = response.data.boards[0].groups;
+        const receivedGroup = groups.find(
+          (group) => group.title === "Received"
+        );
+
+        if (receivedGroup) {
+          setReceivedGroupId(receivedGroup.id);
+        } else {
+          console.error("Received group not found");
+          return null;
+        }
+      } catch (error) {
+        console.error("Error fetching groups:", error);
+        return null;
+      }
+    };
+    getReceivedGroupId();
+  }, [currentBoardId, mondayContext]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (formData.selectedFragrances.length !== 3) {
+      setDisplayLimit(true);
+      return;
+    }
+    setDisableSubmit(true);
+
+    const boardId = currentBoardId;
+    const groupId = receivedGroupId;
+    if (!receivedGroupId) {
+      console.error("No groupId");
+      return;
+    }
+    const columnValues = {
+      sales_associate: `${formData.firstName} ${formData.lastName}`,
+      inscription: formData.inscription,
+      status8: null,
+      scent_profile: formData.selectedFragrances
+        .map((fragrance) => fragrance.label)
+        .join(", "),
+      quantity: formData.quantity,
+    };
+
+    const createItemMutation = `mutation createItem($boardId: ID!, $groupId: String!, $itemName: String!, $columnValues: JSON!) {
+      create_item(board_id: $boardId, group_id: $groupId, item_name: $itemName, column_values: $columnValues) {
+        id
+      }
+    }`;
+    try {
+      await monday.api(createItemMutation, {
+        variables: {
+          boardId,
+          groupId,
+          itemName:
+            formData.orderName === "" ? "New Order" : formData.orderName,
+          columnValues: JSON.stringify(columnValues),
+        },
+      });
+
+      setDisableSubmit(false);
+      setSuccessLabel(true);
+      setFailLabel(false);
+      setDisplayLimit(false);
+      previousOrderRef.current = formData.orderName; // To display the name of the last order submitted
+
+      setFormData({
+        orderName: "",
+        selectedFragrances: [],
+        firstName: "",
+        lastName: "",
+        quantity: "",
+        inscription: "",
+      });
+    } catch (error) {
+      setDisableSubmit(false);
+      setSuccessLabel(false);
+      setFailLabel(true);
+      console.error("Error creating item:", error);
+    }
+  };
+
   return (
-    <Flex direction="Column" align="stretch">
+    <Box
+      border={Box.borders.DEFAULT}
+      rounded={Box.roundeds.MEDIUM}
+      padding={Box.paddings.LARGE}
+      className="start-order-box"
+    >
+      {successLabel && (
+        <Label
+          text={`Successfully submitted order "${previousOrderRef.current}"`}
+        />
+      )}
+      {failLabel && (
+        <Label color="negative" text={`ERROR: Failed to submit order`} />
+      )}
+
       <form onSubmit={handleSubmit}>
-        <Flex direction="Row" justify={"SpaceBetween"}>
-          <TextField requiredAsterisk title="First Name" required />
-          <TextField requiredAsterisk title="Last Name" required />
-          <TextField requiredAsterisk title="Quantity" required />
+        <Flex gap={10} direction="Column" align="stretch">
+          <TextField
+            onChange={(value) => handleTextFieldChange(value, "orderName")}
+            title="Order Name"
+            value={formData.orderName}
+          ></TextField>
+          <Flex gap={10} direction="Row" justify={"SpaceBetween"}>
+            <TextField
+              onChange={(value) => handleTextFieldChange(value, "firstName")}
+              requiredAsterisk
+              title="First Name"
+              required
+              value={formData.firstName}
+            />
+            <TextField
+              onChange={(value) => handleTextFieldChange(value, "lastName")}
+              requiredAsterisk
+              title="Last Name"
+              required
+              value={formData.lastName}
+            />
+            <TextField
+              onChange={(value) => handleTextFieldChange(value, "quantity")}
+              requiredAsterisk
+              title="Quantity"
+              required
+              type="number"
+              value={formData.quantity}
+            />
+          </Flex>
+          {displayLimit && <Label text="Must be exactly 3 fragrances" />}
+          <Dropdown
+            multi
+            placeholder="Choose 3 Fragrances"
+            options={fragranceOptions}
+            className="dropdown-stories-styles_with-chips"
+            onChange={handleSelectChange}
+            value={formData.selectedFragrances}
+          />
+          <TextField
+            onChange={(value) => handleTextFieldChange(value, "inscription")}
+            title="Inscription"
+            value={formData.inscription}
+          ></TextField>
+          <Button disabled={disableSubmit} type="submit">
+            Start Order
+          </Button>
         </Flex>
-        <Label>Hello</Label>
-        <Dropdown label="Fragrances" options={fragranceOptions}></Dropdown>
-        <Button type="submit">Start Order</Button>
       </form>
-    </Flex>
+    </Box>
   );
 };
 
